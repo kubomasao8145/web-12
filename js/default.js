bee948602e1dd30fd05efc53ff426c2d0712b49e
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-06-26 18:40:54 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,17 +2,16 @@
 for(var i=1;i<101;i++){
    var li=document.createElement('li');
   li.textContent=i;
-   if(i%3==0){
-      li.textContent='Fizz'; 
-   
+   if(i%3==0&&i%5==0){
+      li.textContent='FizzBuzz'; 
    }
    else if(i%5==0)
    {
       li.textContent='Buzz'; 
    }
-   else if(i%5==0&&i%3==0)
+   else if(i%3==0)
    {
-     li.textContent='FizzBuzz'; 
+     li.textContent='Fizz'; 
    }
   document.getElementById('FiBuzz').appendChild(li);
 }
