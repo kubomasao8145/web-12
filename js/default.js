89c2c866f06097e804d1918c344d24bb00ab21a1
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-06-26 18:24:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,6 +2,9 @@
 for(var i=0;i<100;i++){
    var li=document.createElement('li');
   li.textContent=i;
+   if(i%3==0){
+      document.GetElementById('li')
+   }
   document.getElementById('FiBuzz').appendChild(li);
 }
 
