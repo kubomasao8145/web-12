25e5f45184b15cde06a236a35577cee1d3ff6c1c
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-06-26 18:27:27 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -3,7 +3,7 @@ for(var i=0;i<100;i++){
    var li=document.createElement('li');
   li.textContent=i;
    if(i%3==0){
-      document.GetElementById('li');
+      document.createElement('li');
       document.getElementById('li')='Fizz'
       
    }
