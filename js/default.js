8cefe34109a29b3ec144f99f56dc0dc3a858dcbd
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-06-26 17:22:50 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,9 +2,13 @@ var li = document.createElement('li');
 li.textContent='アイテム';
 document.getElementById('list').appendChild(li);
 
-for(var i=1;i<100;i++){
+for(var i=1;i<10;i++){
    var li=document.createElement('li');
   li.textContent=i+'回目';
   
   docment.getElementById('list').appendChild(li);
+}
+
+document.getEle,emtById('form').onsubmit=function(){
+  window.alert(document.getElementById('form').word.value'押されました');
 }
