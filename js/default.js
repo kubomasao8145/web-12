f937d6a5208f6f2cf36539baffe7a6f5bae054c2
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-06-26 17:41:58 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,5 +2,10 @@
 
 document.getEle,emtById('form').onsubmit=function(){
   window.alert(document.getElementById('form').word.value'押されました');
- 　
+}
+
+for(var i=0;i<100;1++){
+   var=li document.createElement('li');
+  li.textContent=i;
+  document.getElementById('FiBuzz').appendChild(li);
 }
