8fd656d7c4ebc82e0d198743aa341d1d5000669d
kubomasao8145 <kubomasa.sum41@i.softbank.jp>
2017-06-26 17:31:02 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,10 +1,4 @@
-var li = document.createElement('li');
-li.textContent='アイテム';
-document.getElementById('list').appendChild(li);
 
-for(var i=1;i<10;i++){
-   var li=document.createElement('li');
-  li.textContent=i+'回目';
   
   docment.getElementById('list').appendChild(li);
 }
